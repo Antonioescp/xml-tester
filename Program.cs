a22ce2148fe4773f69c0e2cbf5b042c8bb8b29f7
Antonioescp <packageinstaller2@gmail.com>
2023-08-27 03:01:59 -0600

    Soporte para diff

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System.Xml.Linq;
 using CommandLine;
 using Microsoft.Web.XmlTransform;
+using DiffPlex;
+using DiffPlex.DiffBuilder;
+using DiffPlex.DiffBuilder.Model;
 
 namespace XmlTester
 {
@@ -33,6 +36,10 @@ namespace XmlTester
                 var outputFile = targetFilename + "-transformado" + targetFileExtension;
 
                 MergeXml(options.UniqueFilename, transformationFile, outputFile);
+
+                var before = File.ReadAllLines(options.UniqueFilename).Aggregate((a, b) => a + Environment.NewLine + b);
+                var after = File.ReadAllLines(outputFile).Aggregate((a, b) => a + Environment.NewLine + b);
+                ShowDiff(before, after);
             }
             else if (!string.IsNullOrEmpty(options.InputFilename))
             {
@@ -40,6 +47,10 @@ namespace XmlTester
                     options.InputFilename,
                     options.TransformationFilename,
                     options.OutputFilename);
+
+                var before = File.ReadAllLines(options.InputFilename).Aggregate((a, b) => a + Environment.NewLine + b);
+                var after = File.ReadAllLines(options.OutputFilename).Aggregate((a, b) => a + Environment.NewLine + b);
+                ShowDiff(before, after);
             }
             else
             {
@@ -74,6 +85,32 @@ namespace XmlTester
             }
 
             outputXml.Save(outputFile);
+        }
+
+        public static void ShowDiff(string before, string after)
+        {
+            var diff = InlineDiffBuilder.Diff(before, after);
+
+            var savedColor = Console.ForegroundColor;
+            foreach (var line in diff.Lines)
+            {
+                switch (line.Type)
+                {
+                    case ChangeType.Inserted:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{line.Position} + ");
+                        Console.WriteLine(line.Text);
+                        break;
+                    case ChangeType.Deleted:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write($"{line.Position} - ");
+                        Console.WriteLine(line.Text);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.ForegroundColor = savedColor;
         }
     }
 }
